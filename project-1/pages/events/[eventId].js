3dff08b0541597e70b66b80289b624b46681d0f8
thalita <thalitagq@outlook.com>
2021-08-06 20:51:08 -0300

    feat: :sparkles: Introduce api routes and mongodb

--- project-1/pages/events/[eventId].js
+++ project-1/pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import Comments from "../../components/input/comments";
 
 export default function EventDetailPage(props) {
   const event = props.event
@@ -21,10 +22,7 @@ export default function EventDetailPage(props) {
     <Fragment>
       <Head>
         <title>{event.title}</title>
-        <meta
-          name="description"
-          content={event.description}
-        />
+        <meta name="description" content={event.description} />
       </Head>
       <EventSummary title={event.title} />
       <EventLogistics
@@ -36,6 +34,7 @@ export default function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
--- project-1/pages/index.js
+++ project-1/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import {getFeaturedEvents} from '../helpers/api-util'
 import EventList from '../components/events/event-list'
+import NewsletterRegistration from '../components/input/newsletter-registration'
 
 export default function HomePage(props) {
   return (
@@ -10,6 +11,7 @@ export default function HomePage(props) {
         <title>NextJs Events</title>
         <meta name="description" content="Find a lot of grate events that allow you to evolve..."/>
       </Head>
+      <NewsletterRegistration />
       <EventList items={props.events}/>
     </div>
   )
